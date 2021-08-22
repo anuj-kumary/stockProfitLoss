140d70cbe9addd7dab37f184c58c8c3bdb91d8e2
ANUJ KUMAR YADAV <anujf0510@gmail.com>
2021-08-22 16:04:16 +0530

    fix : mobile UI

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const stockQty = document.querySelector("#stock-quantity");
 const currPrice = document.querySelector("#current-price");
 const checkBtn = document.querySelector("#check-btn");
 const output = document.querySelector('#output');
+const emotion = document.querySelector('.emotion')
 
 function calculateProfitAndLoss() {
     let CP = purchasePrice.value;
@@ -17,20 +18,29 @@ function calculateProfitAndLoss() {
             if (CP > SP) {
                 const loss = (CP - SP) * Qty;
                 const lossPerc = ((CP - SP) * 100) / CP;
-                output.innerText = `You loss is ${loss} and loss Percentage ${lossPerc}% `;
+                output.style.display = 'block'
+                output.innerHTML = `<div style = "border :2px solid #DC2626;width:97%; text-align: center; padding: 1rem .3rem;">
+                Your loss is ${loss} and loss Percentage ${lossPerc}% </div>`;
+                emotion.classList.add('sadTheme')
             }
             //profit
             else if (SP > CP) {
                 const profit = (SP - CP) * Qty;
                 const profitPerc = ((SP - CP) * 100) / CP;
-                output.innerText = `Yay!! Your Profit is ${profit} and profit Percentage is ${profitPerc}`;
+                output.style.display = 'block'
+                emotion.classList.add('happyTheme')
+                output.innerHTML = `<div style = "border :2px solid #6D28D9; width:97%; text-align: center; padding: 1rem .3rem;"> Yay!! Your Profit is ${profit} and profit Percentage is ${profitPerc}%</div>`;
+
             } else {
+                output.style.display = 'block'
                 output.innerText = "No Pain No Gain :)";
             }
         } else {
-            output.innerText = "Please enter value greter then 0";
+            output.style.display = 'block'
+            output.innerText = "Please enter valid value";
         }
     } else {
+        output.style.display = 'block'
         output.innerText =
             "Please enter values greater than 0 only numbers are allowed in above fields";
     }
